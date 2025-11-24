d4ecafaa7e1ff04d9af14a97ed688fb1131abe55
Alvaro Echevarria Cuesta <alvaroecuesta@gmail.com>
2025-11-24 11:45:39 -0500

    basic outlines

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,26 @@
+import { Card } from "@/components/ui/card";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black"></div>
+    <div className="min-h-screen">
+      <div className="h-full min-h-screen max-w-5xl mx-auto flex flex-col p-4">
+        <h1 className="text-2xl font-bold">Videos</h1>
+        <div className="flex items-center gap-2">
+          
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
+          <Card className="bg-blue-100 min-h-[200px]">
+            <div className="p-4">
+              <p>Card 1</p>
+            </div>
+          </Card>
+          <Card className="bg-green-100 min-h-[200px]">
+            <div className="p-4">
+              <p>Card 2</p>
+            </div>
+          </Card>
+        </div>
+      </div>
+    </div>
   );
 }
